53e3cc27cb8165db4d8b82325cb5abe4cbdcf110
Abdulrasheed <jamaalokh@gmail.com>
2022-09-13 02:33:58 +0100

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - Entry Point
+ * main - gives a random number to int n everytime it executes, and prints it
  *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
 	int n;
-	int gp;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	gp = (n % 10);
-	if (n > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, gp);
-	if (n == 0)
-		printf("Last digit of %d is %d and is 0\n", n, gp);
-	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, gp);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if
+		printf("%d is negative\n", n);
 	return (0);
 }
