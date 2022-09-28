4dc1dbd86402d2b0a5b80dc155585b18b88ad203
Abdulrasheed <jamaalokh@gmail.com>
2022-09-28 22:14:13 +0100

    raising a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - is a function that returns the value of a number raised
+ * to the power of another number
+ * @x: the number to raise
+ * @y: the value of the raise
+ * Return: the value of x to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
